af039e76344a61f7ec98792e968e044bcc4f945c
lfcoelloc <lfcoelloc@gmail.com>
2026-01-07 18:54:49 -0500

    testcommit

---
+++ android/app/src/main/kotlin/com/example/parcialpucetec2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.parcialpucetec2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/crear.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Crear extends StatelessWidget {
+  const Crear({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
